bc84acf82cbff3781c2710d1e9e9987684a6c7cf
felipedavisvitalmed <fdsilva@vitalmed.com.br>
2025-04-06 17:05:46 -0300

    aula 31

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car,Marca
+
 # Register your models here.
+
+class MarcaAdmin(admin.ModelAdmin):
+        list_display = ('id', 'name')
+        search_fields = ('id','name')
+
+
+
+
+
 class CarAdmin(admin.ModelAdmin):
         list_display = ('model', 'marca','factory_year','model_year','value')
         search_fields = ('model','marca')
 
 
+admin.site.register(Marca, MarcaAdmin)
 admin.site.register(Car, CarAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 
     
-  
+
+class Marca(models.Model):
+        id     = models.AutoField(primary_key=True)
+        name   = models.CharField(max_length=200)
+
+        def __str__(self):
+         return self.name
+               
+
 class Car(models.Model):
         id = models.AutoField(primary_key=True)
         model = models.CharField(max_length=200)
-        marca = models.CharField(max_length=200)
+        marca = models.ForeignKey(Marca,on_delete=models.PROTECT, related_name='car_marca')
         factory_year = models.IntegerField(blank=True , null=True)
         model_year = models.IntegerField(blank=True , null=True)
+        plate = models.CharField(max_length=10, blank=True,null=True)
         value = models.FloatField(blank=True , null=True)
+        photo = models.ImageField(upload_to='cars/',blank=True , null=True)
 
 
         def __str__(self):
          return self.model
+        
+
+
