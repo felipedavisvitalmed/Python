2fcb260bead625d045629002b9638a886100fa6c
felipedavisvitalmed <fdsilva@vitalmed.com.br>
2025-04-09 21:17:28 -0300

    ORM

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from cars.models import Car
 
 
 def cars_view(request):
-    return  HttpResponse('Meus carros')
+     cars = Car.objects.all()
+
+     return render(
+          request,
+          'cars.html',
+          { 'cars': cars  }
+          )
