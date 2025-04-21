c8247f18ec9b11815190c599d4a0b902e1baa8fc
felipedavisvitalmed <fdsilva@vitalmed.com.br>
2025-04-21 20:33:04 -0300

    Recriando as views de Cars para classes

--- cars/views.py
+++ cars/views.py
@@ -1,35 +1,40 @@
 from django.shortcuts import render,redirect
 from cars.models import Car
 from cars.forms import CarMordelForm
+from django.views import View
 
 
 
-def cars_view(request):
-     cars = Car.objects.all().order_by('model')
-     search = request.GET.get('search')
-     print(search)
-     if search:
-          cars = Car.objects.filter(model__icontains = search).order_by('model')
+class CarsView(View): 
+     def get(self,request):
+          cars = Car.objects.all().order_by('model')
+          search = request.GET.get('search')
+          print(search)
+          if search:
+               cars = Car.objects.filter(model__icontains = search).order_by('model')
 
-     return render(
-          request,
-          'cars.html',
-          { 'cars': cars  }
-          )
-
-
-def new_car_view(request):
-
-     if request.method =='POST':
-          new_car_form = CarMordelForm(request.POST,request.FILES)
-          if new_car_form.is_valid():
-               new_car_form.save()
-               return redirect('cars_list')
-          
-     else:     
-          new_car_form = CarMordelForm()
-     return render(
+          return render(
                request,
-               'new_car.html',
-               {'new_car_form': new_car_form}
-          )
+               'cars.html',
+               { 'cars': cars  }
+               )
+
+
+
+class NewCarView(View):
+    template_name = 'new_car.html'
+
+    def render_form(self, request, form):
+        """Método auxiliar para renderizar o formulário."""
+        return render(request, self.template_name, {'new_car_form': form})
+
+    def get(self, request):
+        new_car_form = CarMordelForm()
+        return self.render_form(request, new_car_form)
+
+    def post(self, request):
+        new_car_form = CarMordelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+        return self.render_form(request, new_car_form)
